139b5b0d89647e7cc54c7518810b786f0b3dafb7
Bibia Saturno <biancapatrocinio774@gmail.com>
2026-03-12 01:56:18 -0300

    feat: build do jogo, v1

--- GameAI/Assets/Script/BlinkyLobbyInteract.cs
+++ GameAI/Assets/Script/BlinkyLobbyInteract.cs
@@ -7,7 +7,7 @@ public class BlinkyLobbyInteract : MonoBehaviour
     public GameObject blinkyLobbyPanel; // Drag the Blinky text box here
     
     [Header("Scene Configuration")]
-    public string lobbySceneName = "Lobby"; // Type the EXACT name of the Lobby scene here
+    public string lobbySceneName; // Type the EXACT name of the Lobby scene here
 
     private void Start()
     {
@@ -38,7 +38,7 @@ public class BlinkyLobbyInteract : MonoBehaviour
     public void ReturnToLobby()
     {
         // Loads the Lobby scene
-        SceneManager.LoadScene("Lobby"); 
+        SceneManager.LoadScene(lobbySceneName); 
     }
 
     public void ClosePanel()
